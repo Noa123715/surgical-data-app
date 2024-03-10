9eba9dc98b8558b44bdddc3992aaddcf17af0a7a
noa123715 <noa123715@gmail.com>
2024-03-11 00:10:23 +0200

    start to calculate amountOfStaff

---
+++ server/amountOfStaff.js
@@ -0,0 +1,17 @@
+import * as getData from "./getData.js";
+
+function getStaffPerRoom(surgerys) {
+    let staff = surgerys.map(surgery => surgery.staff);
+    //להבין איך המערך בנוי ולהוריד כפילויות
+    return staff;
+}
+
+export default async function amountOfStaff(date) {
+    const daySurgery = await getData.getDailySurgery(date);
+    console.log(daySurgery);
+    const surgeryPerRoom = getData.getSurgeryPerRoom(daySurgery);
+    const staffPerRoom = surgeryPerRoom.map(room => {
+
+    });
+    return staffPerRoom;
+}
